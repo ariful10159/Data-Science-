0c3ff7ac4079e1294259e7da2e2e34c0fe05f77c
Ariful islam Masum <arifulislammasum97@gmail.com>
2025-02-15 02:59:08 +0600

    Add files via upload

---
+++ Pandasc/import_pandas.py
@@ -0,0 +1,14 @@
+import pandas as pd 
+
+mydataset = {
+  'cars': ["BMW", "Volvo", "Ford"],
+  'Numbers': [3, 7, 2]
+}
+
+myvar = pd.DataFrame(mydataset)
+
+print(myvar) 
+
+# checking pandas version 
+
+print ( pd.__version__)
